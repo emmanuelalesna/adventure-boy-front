c520d56e6a13205a50c9674436606fd5bc1d3813
Ian <ianbraverman@hotmail.com>
2024-08-06 15:44:27 -0400

    working on login more

--- Pages/LoginRegisterScreen/index.js
+++ Pages/LoginRegisterScreen/index.js
@@ -17,7 +17,39 @@ userRegisterForm.addEventListener("submit", function (e) {
   })
     .then((res) => res.json())
     .then((resBody) => {
-      let displayedLoginInfo = document.getElementById("RegisterResponse");
-      displayedLoginInfo.innerText = JSON.stringify(resBody);
+      let displayedRegisterInfo = document.getElementById("RegisterResponse");
+      displayedRegisterInfo.innerText = JSON.stringify(resBody);
     });
+});
+
+const userLoginForm = document.getElementById("loginUser");
+
+userLoginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let checkLogin;
+
+  const loginUser = {
+    Username: e.target.elements["usernameLogin"],
+    Password: e.target.elements["passwordLogin"],
+  };
+
+  fetch(
+    "http://localhost:5114/api/account/" + e.target.elements["usernameLogin"]
+  )
+    .then((res) => res.json())
+    .then((resBody) => (checkLogin = resBody));
+
+  if (checkLogin == null) {
+    let displayedLoginInfo = document.getElementById("LoginResponse");
+    displayedLoginInfo.innerText = "Incorrect Username!";
+  }
+
+  if (checkLogin.Password == e.target.elements["passwordLogin"]) {
+    let displayedLoginInfo = document.getElementById("LoginResponse");
+    displayedLoginInfo.innerText = JSON.stringify(checkLogin);
+  } else {
+    let displayedLoginInfo = document.getElementById("LoginResponse");
+    displayedLoginInfo.innerText = "Incorrect Password!";
+  }
 });
