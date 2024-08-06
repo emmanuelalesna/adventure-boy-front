f3a2fbe6e72990cdabfc73cbcd872b17ea8abfaa
emmanuelalesna <emmanuelalesna@gmail.com>
2024-08-06 17:10:23 -0400

    fixed login

--- Pages/LoginRegisterScreen/index.js
+++ Pages/LoginRegisterScreen/index.js
@@ -31,17 +31,23 @@ userLoginForm.addEventListener("submit", function (e) {
   const passwordLogin = e.target.elements["passwordLogin"].value;
 
   fetch("http://localhost:5114/api/account/" + usernameLogin)
-    .then((res) => res.json())
-    .then((resBody) => {
-      if (!resBody) {
+    .then((res) => {
+      if (res.status == 404) {
         let displayedLoginInfo = document.getElementById("LoginResponse");
         displayedLoginInfo.innerText = "Incorrect Username!";
-      } else if (resBody.Password == passwordLogin) {
-        let displayedLoginInfo = document.getElementById("LoginResponse");
-        displayedLoginInfo.innerText = JSON.stringify(checkLogin);
-      } else {
-        let displayedLoginInfo = document.getElementById("LoginResponse");
-        displayedLoginInfo.innerText = "Incorrect Password!";
+        return 404;
+      }
+      return res.json();
+    })
+    .then((resBody) => {
+      if (resBody != 404) {
+        if (resBody.password == passwordLogin) {
+          let displayedLoginInfo = document.getElementById("LoginResponse");
+          displayedLoginInfo.innerText = JSON.stringify(resBody);
+        } else {
+          let displayedLoginInfo = document.getElementById("LoginResponse");
+          displayedLoginInfo.innerText = "Incorrect Password!";
+        }
       }
     });
 });
