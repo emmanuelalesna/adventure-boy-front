e24a7bd6e485caca4f02bdbbda444e2b8c679445
Ian <ianbraverman@hotmail.com>
2024-08-07 15:06:20 -0400

    started fight screen

--- Pages/MainGameScreen/index.js
+++ Pages/MainGameScreen/index.js
@@ -1,4 +1,5 @@
 let currentPlayer = null;
+//will request.js
 
 async function getPlayer() {
   await fetch("http://localhost:5114/api/player/1")
@@ -6,5 +7,28 @@ async function getPlayer() {
     .then((resBody) => (currentPlayer = resBody));
   let playerInfo = document.getElementById("PlayerInfo");
   playerInfo.innerText = currentPlayer.name;
-
 }
+
+function checkRoomIn(room) {
+  switch (room) {
+    case 1:
+      playRoomOne();
+      break;
+  }
+}
+
+function playRoomOne() {
+  //needs to set image to the image associated with the room
+  let isPlaying = true;
+  while ((isPlaying = true)) {
+    let displayRoomInfo = document.getElementById("displayGameText");
+    let generateContinueButtonSection = document.getElementById(
+      "generateContinueTextButton"
+    );
+    displayRoomInfo.innerText = "You enter section 1";
+
+    isPlaying = false;
+  }
+}
+
+const continueButton = document.getElementById("");
