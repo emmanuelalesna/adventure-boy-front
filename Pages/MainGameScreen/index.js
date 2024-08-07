38887fe5f4137cf25fbb078248d2fb8106564232
emmanuelalesna <emmanuelalesna@gmail.com>
2024-08-07 12:57:16 -0400

    added retrieving images from scryfall

--- Pages/MainGameScreen/index.js
+++ Pages/MainGameScreen/index.js
@@ -6,5 +6,15 @@ async function getPlayer() {
     .then((resBody) => (currentPlayer = resBody));
   let playerInfo = document.getElementById("PlayerInfo");
   playerInfo.innerText = currentPlayer.name;
+}
 
+async function getRoom() {
+  const roomUrl = await fetch("http://localhost:5114/api/room/-1")
+    .then((res) => res.json())
+    .then((resbody) => resbody.imageUrl);
+  let cardArt = await fetch(roomUrl)
+    .then((res) => res.json())
+    .then((resBody) => resBody.image_uris.art_crop);
+  document.getElementById("card_art").src = cardArt;
+  // document.body.style.backgroundImage = `url(${cardArt})`
 }
