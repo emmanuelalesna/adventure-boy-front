54b48a89014d838bef9fc4fc851e2ab8e3e2d997
emmanuelalesna <emmanuelalesna@gmail.com>
2025-05-07 13:20:49 -0400

    removing /dist from remote

--- dist/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-import { registerUser, loginUser, logoutUser } from "/assets/js/loginRegisterMethods.js";
-
-document.getElementById("logoutButton").addEventListener("click", logoutUser);
-
-document
-  .getElementById("registerUser")
-  .addEventListener("submit", registerUser);
-
-document.getElementById("loginUser").addEventListener("submit", loginUser);
