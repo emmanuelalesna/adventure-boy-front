38dc6db4893e0d7f088524a23543482a69626d30
emmanuelalesna <emmanuelalesna@gmail.com>
2025-05-08 01:13:29 -0400

    moved login request handler into index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,40 @@
-import {
-  registerUser,
-  loginUser,
-  logoutUser,
-} from "./loginRegisterMethods.js";
+import { logoutUser, registerUser } from "./loginRegisterMethods.js";
+import { getItems, getSpells, getEnemies, getRooms } from "./getMethods.js";
+import { loginRequest } from "./loginRegisterRequests.js";
+import apiUrl from "./url.js";
+
+const { urlBoy, accountEndpoint } = apiUrl;
+
+// login user
+const loginUser = async (e) => {
+  e.preventDefault();
+  const user = {
+    username: e.target.elements[0].value,
+    password: e.target.elements[1].value,
+  };
+  let displayedLoginInfo = document.getElementById("LoginResponse");
+  try {
+    const loginResponse = await loginRequest(user);
+    if (loginResponse.ok) {
+      const resBody = await loginResponse.json();
+      displayedLoginInfo.innerText =
+        'Login successful! Start fight by pressing the "To fight screen" button.';
+      localStorage.setItem("currentAccount", JSON.stringify(resBody));
+      document.getElementById("toFightScreen").hidden = false;
+      document.getElementById("registerFormDiv").hidden = true;
+      getItems();
+      getSpells();
+      getEnemies();
+      getRooms();
+    } else {
+      displayedLoginInfo.innerText =
+        "Error: incorrect username and/or password.";
+      throw new Error(`${loginResponse.status}: ${loginResponse.statusText}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 document.getElementById("logoutButton").addEventListener("click", logoutUser);
 
--- public/assets/js/loginRegisterMethods.js
+++ public/assets/js/loginRegisterMethods.js
@@ -1,40 +1,7 @@
-import { getItems, getSpells, getEnemies, getRooms } from "./getMethods.js";
-import { loginRequest } from "./loginRegisterRequests.js";
 import { createPlayer } from "./playerMethods.js";
 import apiUrl from "./url.js";
 
 const { urlBoy, accountEndpoint } = apiUrl;
-
-// login user
-const loginUser = async (e) => {
-  e.preventDefault();
-  const user = {
-    username: e.target.elements[0].value,
-    password: e.target.elements[1].value,
-  };
-  let displayedLoginInfo = document.getElementById("LoginResponse");
-  try {
-    const loginResponse = await loginRequest(user);
-    if (loginResponse.ok) {
-      const resBody = await loginResponse.json();
-      displayedLoginInfo.innerText =
-        'Login successful! Start fight by pressing the "To fight screen" button.';
-      localStorage.setItem("currentAccount", JSON.stringify(resBody));
-      document.getElementById("toFightScreen").hidden = false;
-      document.getElementById("registerFormDiv").hidden = true;
-      getItems();
-      getSpells();
-      getEnemies();
-      getRooms();
-    } else {
-      displayedLoginInfo.innerText =
-        "Error: incorrect username and/or password.";
-      throw new Error(`${loginResponse.status}: ${loginResponse.statusText}`);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 //register user
 const registerUser = async (e) => {
@@ -68,4 +35,4 @@ const logoutUser = () => {
   localStorage.clear();
 };
 
-export { registerUser, loginUser, logoutUser };
+export { registerUser, logoutUser };
