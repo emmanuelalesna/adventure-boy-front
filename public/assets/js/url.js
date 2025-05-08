0f3b526e0ab7175e0adafb189464e96a78f4d586
emmanuelalesna <emmanuelalesna@gmail.com>
2025-05-08 02:26:49 -0400

    changed url

--- public/assets/js/url.js
+++ public/assets/js/url.js
@@ -1,6 +1,6 @@
 export default {
-  urlBoyBak: "https://adventure-boy-back-2-dphugvgsh7hbdxh2.eastus2-01.azurewebsites.net/",
-  urlBoy: "http://localhost:5114/",
+  urlBoy: "https://adventure-boy-back-2-dphugvgsh7hbdxh2.eastus2-01.azurewebsites.net/",
+  urlDev: "http://localhost:5114/",
   itemEndpoint: "api/item/",
   spellEndpoint: "api/spell/",
   enemyEndpoint: "api/enemy/",
