fcaa6747e09f3275ac309679f02a449dc42695df
emmanuelalesna <emmanuelalesna@gmail.com>
2025-05-24 03:27:56 -0400

    added individual player get

--- src/js/playerRequests.ts
+++ src/js/playerRequests.ts
@@ -1,9 +1,11 @@
 import type { Player } from "./Player";
 import { urlBoy, playerEndpoint } from "./url.json";
 
+const token = localStorage.getItem("token");
+const id = localStorage.getItem("id");
+
 const createPlayerRequest = async (player: Player) => {
   const { AccountId, Name, CurrentHealth, CurrentMana } = player;
-  const token = localStorage.getItem("token");
   if (AccountId && Name && token) {
     return fetch(urlBoy + playerEndpoint, {
       method: "POST",
@@ -46,18 +48,27 @@ const updatePlayer = async (
   await req;
 };
 
-const getPlayers = async () => {
-  const token = localStorage.getItem("token");
-  const id = localStorage.getItem("id");
+const getPlayer = async (playerId: string) => {
   if (token && id) {
-    return fetch(urlBoy + playerEndpoint + id, {
+    const params = new URLSearchParams().append("playerId", playerId);
+    return fetch(urlBoy + playerEndpoint + id + `?${params}`, {
+      headers: { Authorization: "Bearer " + JSON.parse(token).accessToken },
+    });
+  } else {
+    throw new Error("token and/or id is missing");
+  }
+};
+
+const getPlayers = async () => {
+  if (token && id) {
+    return fetch(urlBoy + playerEndpoint + id + "/all", {
       headers: {
         Authorization: "Bearer " + JSON.parse(token).accessToken,
       },
     });
   } else {
-    throw new Error("token and id is required");
+    throw new Error("token and/or id is missing");
   }
 };
 
-export { createPlayerRequest, updatePlayer, getPlayers };
+export { createPlayerRequest, updatePlayer, getPlayer, getPlayers };
